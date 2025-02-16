8816e29627a2b862b21651922c98fdea1fedce21
Sampath950 <164990569+SampathKumar950@users.noreply.github.com>
2025-02-16 10:14:00 +0530

    Update Api.jsx

--- ecommerce/src/assets/Api.jsx
+++ ecommerce/src/assets/Api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';  // Import the global base URL
 
 // Creating an instance of axios with the base URL
 const Api = axios.create({
-  baseURL: "http://localhost:3000",  // This is your global URL
+  baseURL: "https://ecommerce-backend-2rix.onrender.com",  // This is your global URL
 });
 
 export default Api;
