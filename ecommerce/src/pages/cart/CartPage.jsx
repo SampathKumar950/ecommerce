c4ca7d1a32849c58549593b5609350d9e50f7789
Sampath950 <164990569+SampathKumar950@users.noreply.github.com>
2025-02-18 00:21:41 +0530

    Update CartPage.jsx

--- ecommerce/src/pages/cart/CartPage.jsx
+++ ecommerce/src/pages/cart/CartPage.jsx
@@ -37,7 +37,7 @@ const CartPage = () => {
           Authorization:`Bearer ${token}`,
         }
       })
-      navigate('/home');
+      navigate('/');
     }catch(error){
       console.log(error.message);
     }
